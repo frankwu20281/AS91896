5341f0fe220945c2d34cbd9ca65fd1c94f434d4a
frankwu20281 <135084769+frankwu20281@users.noreply.github.com>
2023-07-18 20:47:19 +1200

    Update AS91896program.py

--- AS91896 - Frank Wu/AS91896program.py
+++ AS91896 - Frank Wu/AS91896program.py
@@ -1,6 +1,5 @@
 from tkinter import * 
 from tkinter import ttk, messagebox
-from random import randint
 import json
 
 class MainWindow: #class running the whole program 
@@ -11,12 +10,12 @@ class MainWindow: #class running the whole program
            
         print(self.receipt_list)
         self.index = -1 if not self.receipt_list else 0  #counter to keep count what receipt in the list the program currently is displaying/using, start at -1 because 0 is the item in list
-        self.page_amount = 0 if not self.receipt_list else len(self.receipt_list)    #keep count of the total amount of receipts in the program 
+        self.page_amount = len(self.receipt_list)    #keep count of the total amount of receipts in the program 
         
         #config the main program window
         self.master = master 
         root.title("Receipts")
-        root.geometry("700x400")
+        root.geometry("700x450")
         self.style = ttk.Style(root)
         self.style.theme_use("xpnative")
         
@@ -59,8 +58,10 @@ class MainWindow: #class running the whole program
         
         #program title and system mode button 
         ttk.Label(root, text= "Item Hire Tracker" ,  font= ("Arial", 20, 'bold')). pack (pady= 10)
+
         self.mode_button = ttk.Button(root, text= "Light mode", command= lambda :self.themes("Dark" if self.mode == "Light" else "Light"))
         self.mode_button. pack(anchor= E, padx = 70,   expand= TRUE)
+        
         
         #creating 'master' frame to put all ui stuff in
         
@@ -68,7 +69,7 @@ class MainWindow: #class running the whole program
         frame.pack( padx= 20,pady= 10, expand= TRUE)
         
         #frame with all the user entry stuff 
-        self.EntryFrame=ttk.LabelFrame(frame, text= "fill out", style= "sub.TFrame")
+        self.EntryFrame=ttk.LabelFrame(frame, text= "Fill Out", style= "sub.TFrame")
         self.EntryFrame.pack(side= LEFT, fill= BOTH, **padding) 
 
         self.name_label = ttk.Label(self.EntryFrame, text= "enter full name").grid(row=0, column=0, pady= 5, padx= 5)
@@ -89,32 +90,39 @@ class MainWindow: #class running the whole program
         
         ttk.Button(self.EntryFrame, text = "Return", command = self.delete).grid(row=3,column=0, pady= 5)
 
+        ttk.Button(self.EntryFrame, text= "Search All", command= self.search_window).grid(row =4, column= 0, columnspan= 2, padx=5, pady = 5)
         # frame with the receipt search bar 
         SearchFrame = ttk.LabelFrame(frame, text= "Search Receipt Number", style= "sub.TFrame")
         SearchFrame.pack(fill= X ,**padding)
         
-        self.search_entry = ttk.Entry(SearchFrame)
-        self.search_entry.bind("<KeyRelease>", (lambda event: self.receipt_number_search(self.search_entry.get())))
-        self.search_entry.pack(side= LEFT, expand= True, padx = 5)
+        ttk.Label(SearchFrame,text="Receipt:").pack(side=LEFT, padx= 5)
         
-        ttk.Button(SearchFrame, text= "Search All", command= self.search_window).pack(side= LEFT ,padx=5, pady = 5)
+        self.search_entry = ttk.Entry(SearchFrame)
+        
+        self.search_entry.bind("<KeyRelease>", (lambda event: self.receipt_number_search(self.search_entry.get())))
+        self.search_entry.pack(side= LEFT, expand= TRUE, fill=X, padx = 5 , pady= 5)
+        
+        
    
         
         #frame with the receipt display stuff 
-        DisplayFrame=ttk.LabelFrame(frame, text= "Receipt", style= "sub.TFrame")
+        DisplayFrame=ttk.LabelFrame(frame, text= "Receipt Display", style= "sub.TFrame")
         DisplayFrame.pack(**padding, expand=TRUE,fill = BOTH)
-
+        
+        
+        
         self.receipt_image = ttk.Label(DisplayFrame, text="No receipts\n\n\n\n")
-        self.receipt_image.pack( anchor= CENTER,expand= TRUE)
+        self.receipt_image.pack( anchor= CENTER,expand= TRUE, pady= 5)
+        ttk.Button(DisplayFrame, text= "Search All", command= self.search_window).pack( anchor= CENTER,expand= TRUE, ipadx= 10)
 
         self.back = ttk.Button(DisplayFrame, text="←", command= lambda  :self.nav("left"), state= DISABLED)
-        self.back.pack( side= LEFT, expand= TRUE ,anchor= CENTER, pady=10)
+        self.back.pack( side= LEFT, expand= TRUE ,anchor= CENTER, pady=10, padx= 5)
 
         self.page_number = ttk.Label(DisplayFrame, text= f"{self.index+1} of {self.page_amount}")
         self.page_number.pack(side= LEFT, expand= TRUE,  anchor= CENTER)
         
         self.forward = ttk.Button(DisplayFrame, text= "→", command=lambda  : self.nav("right"), state= DISABLED)
-        self.forward.pack( side= LEFT, expand= TRUE ,anchor= CENTER, pady=10)
+        self.forward.pack( side= LEFT, expand= TRUE ,anchor= CENTER, pady=10, padx= 5)
         
         if self.receipt_list: #checks if program started with any preloaded infomation in the order list, and updates the program to show this info
             self.receipt_shifting()
@@ -123,28 +131,50 @@ class MainWindow: #class running the whole program
     def search_window (self):   #function to initiate advanced search window 
         
         #window config stuff
+        global SearchWindow
         SearchWindow = Toplevel(root)
         SearchWindow.title('Advanced Search')
         SearchWindow.configure(bg= "SystemButtonFace" if self.mode == "Light" else "#2b2b2b" )
-        style = ttk.Style(SearchWindow)
-        style.theme_use("xpnative")
-        style.configure("TCombobox", foreground = "black")
-
+        self.searchwindowstyle = ttk.Style(SearchWindow)
+        self.searchwindowstyle.theme_use("xpnative")
+        self.searchwindowstyle.configure("Treeview", foreground = "black")
+        self.searchwindowstyle.configure("Treeview.Heading", foreground="black")
+        self.sort_direction = True
         #Title and label for the window
-        Label(SearchWindow, text='Search In All Receipts', font = ("Arial", 15, "bold"), foreground= "black" if self.mode == "Light" else "white", 
-              background="SystemButtonFace" if self.mode == "Light" else "#2b2b2b" ).pack(pady= 10 )
-        Label(SearchWindow, text= 'Double click to select:').pack()
+        ttk.Label(SearchWindow, text='Search In All Receipts', font = ("Arial", 15, "bold") ).pack(pady= 10 )
+        ttk.Label(SearchWindow, text= 'Search for anything and click to display on main window:').pack()
 
         #entry box for user to enter their search, binded to keyrelease so program will run search_choice function every time user stops typing  
         self.adv_search_entry = ttk.Entry(SearchWindow)
         self.adv_search_entry.bind ("<KeyRelease>", lambda event: self.all_search( self.adv_search_entry.get().strip()))
-     
         self.adv_search_entry.pack(padx= 10, fill= X)
-        #listbox to show users search results, when user double clicks on option it displays it on main window
-        self.receipt_storage = Listbox(SearchWindow, width= 50, background= "SystemButtonFace" if self.mode == "Light" else "#2b2b2b", 
-                                       foreground= "black" if self.mode == "Light" else "white")
-        self.receipt_storage.bind('<Double-Button>', (lambda event: self.double_click(self)))
-        self.receipt_storage.pack(pady= 10, padx=10, fill= BOTH, expand= TRUE, side= LEFT)
+        
+        columns= ('Receipt', 'Name', 'Item', 'Amount')
+        
+        scrollbar = Scrollbar(SearchWindow)
+        self.tree_table = ttk.Treeview(SearchWindow, columns=columns, show = 'headings',yscrollcommand = scrollbar.set)
+        scrollbar.config(command = self.tree_table.yview)
+        scrollbar.pack(side= RIGHT, fill= Y, pady= 10)
+        
+        self.tree_table.heading('Receipt', text = "Receipt number", command= lambda: self.sorting('Receipt', False if self.sort_direction  else True))
+        self.tree_table.column('Receipt', anchor='center', minwidth=50)
+        
+        self.tree_table.heading('Name', text = "Full Name", command= lambda: self.sorting('Name', False if self.sort_direction else True))
+        self.tree_table.column('Name', anchor='center', minwidth=50)
+        
+        self.tree_table.heading('Item', text = "Item", command= lambda: self.sorting('Item', False if self.sort_direction else True))
+        self.tree_table.column('Item', anchor='center', minwidth=50)
+        
+        self.tree_table.heading('Amount', text = "Amount", command= lambda: self.sorting('Amount', False if self.sort_direction else True))
+        self.tree_table.column('Amount', anchor='center', minwidth=50)
+        
+        self.tree_table.pack(pady= 10, padx=10, fill= BOTH, expand= TRUE, side= LEFT)
+        
+        
+        
+        #listbox to show users search se "white")
+        self.tree_table.bind('<<TreeviewSelect>>', (lambda event: self.double_click(self)))
+        
         
         self.all_search(self.adv_search_entry.get().strip())
 
@@ -165,32 +195,68 @@ class MainWindow: #class running the whole program
 
         self.style.configure('TButton', font = (None, 12), foreground = "#262626"if mode == 'Dark' else "Black")
 
+        try: 
+            self.searchwindowstyle.configure('TLabel', foreground= "black" if self.mode == "Light" else "white", 
+              background= "SystemButtonFace" if self.mode == "Light" else "#2b2b2b")
+            SearchWindow.configure(bg= "SystemButtonFace" if self.mode == "Light" else "#2b2b2b" )
+            
 
+        except: 
+            pass 
+
+    def sorting (self, selected, sort_direction): 
+        self.sort_direction = sort_direction
+        print(self.sort_direction)
+        data = [(int(self.tree_table.set(child, selected))if selected == "Receipt" or selected == "Amount" else (self.tree_table.set(child, selected) ), child) for child in self.tree_table.get_children('')]
+        print(data,'before')
+        data.sort(reverse= sort_direction)
+        print(data, 'after')
+        for index, (value, child) in enumerate(data):
+            print (index, 'index-', child , "child")
+            self.tree_table.move(child, '', index)
+
+        for column in self.tree_table['columns']:
+            if column != selected:self.tree_table.heading(column, text=column)
+            else:
+                arrow = '↑' if  sort_direction else '↓'
+                self.tree_table.heading(column, text=f'{column} {arrow}')
+        
+        
+        
+        
+        
+        
     def all_search(self, typed):   #search all function for the advanced search window    
         print('allsearchstart')
-        search_list =[] 
-        self.receipt_storage.delete(0, END)
-        for dicts in self.receipt_list: 
-            print(dicts, "- receipt list")
-            item_text = f"Name: {dicts['name'].title()} - Receipt: {dicts['receipt']} - Item: {dicts['item'].title()} - Amount: {dicts['amount']}"
-            search_list.append(item_text)   #add receipt list to receipt display listbox
+        
+        for row in self.tree_table.get_children(): 
+            self.tree_table.delete(row)
+        
+        
             
-        for search in search_list:  #seeing if user search matches anything in receipt list
+            
+        for search in self.receipt_list:  #seeing if user search matches anything in receipt list
             if typed == "":     #if user has typed nothing then display all receipts in listbox 
-                self.receipt_storage.insert(END, search)
-            elif typed.lower() in search.lower():   # if user has typed something and it matches something in a receipt, display it in listbox
-                self.receipt_storage.insert(END, search)
-        if not self.receipt_storage.get(0): #if user search doesnt match anything, tell user there are no search results
+                print(self.receipt_list)
+            
+                self.tree_table. insert ('', END, values = (search['receipt'],search['name'].title(),search['item'].title(),search['amount']))
+            elif typed.lower() in str(search['receipt']) or typed.lower() in search['name'] or typed.lower() in search['item']or typed.lower() in search['amount']:   # if user has typed something and it matches something in a receipt, display it in listbox
+                self.tree_table. insert ('', END, values = (search['receipt'],search['name'].title(),search['item'].title(),search['amount']))
+
+        if not self.tree_table.get_children(0): #if user search doesnt match anything, tell user there are no search results
             print('no')
-            self.receipt_storage.insert(END, "No Search Results")  
+            self.tree_table. insert ('', END, values = ('No Results','-','-',''))
+        
+    
             
     
             
     def double_click (self, event): #updates main window to display the order that the user has selected in the advanced search window
-        selected_receipt = self.receipt_storage.get(self.receipt_storage.curselection()) #gets the string that the user has selected 
-        receipt_number = int(selected_receipt[selected_receipt.find("Receipt")+9: selected_receipt.find("Item")-3].strip()) # in that string, it extracts the receipt number displayed in the string
-        print (receipt_number)
-        self.index = next((index for (index, dict) in enumerate(self.receipt_list) if dict["receipt"] == receipt_number), None) #searches for the dictionary that matches the receipt number, then finds the index of that dictionary in the self.receipt_list list
+         #gets the string that the user has selected 
+        selected_receipt = self.tree_table.item(self.tree_table.selection())
+        print(selected_receipt,'asdfasdfasdfasdfasdf')
+        
+        self.index = next((index for (index, dict) in enumerate(self.receipt_list) if dict["receipt"] == selected_receipt['values'][0]), None) #searches for the dictionary that matches the receipt number, then finds the index of that dictionary in the self.receipt_list list
         self.receipt_shifting() #updates the main window to display the order
     
     def receipt_number_search (self, search_field ): #function to search for orders using receipt number 
@@ -216,7 +282,7 @@ class MainWindow: #class running the whole program
         print(self.receipt_list)
         
         #updates the self.receipt_image Label on the main window to display the current order 
-        self.receipt_image.config(text= f"Store \nName: {receipt_order['name'].title()}\nReciept: {str(receipt_order['receipt']).zfill(6)}\nItem: {receipt_order['item'].title()}\nAmount: {receipt_order['amount']}") 
+        self.receipt_image.config(text= f"Name: {receipt_order['name'].title()}\nReciept: {str(receipt_order['receipt']).zfill(6)}\nItem: {receipt_order['item'].title()}\nAmount: {receipt_order['amount']}") 
         
         #updates nav buttons according to the position of the displayed order in the self.receipt_list 
         self.forward.config(state= DISABLED if self.page_amount == 1 and self.index ==0 or self.index+1 == self.page_amount else ACTIVE)
@@ -262,6 +328,8 @@ class MainWindow: #class running the whole program
             self.name_entry.configure(foreground= "black"), self.item_entry.configure(foreground= "black"), self.amount_entry.configure(foreground= "black")
             self.receipt_shifting()
             self.write_json()
+            try: self.all_search( self.adv_search_entry.get().strip())
+            except: pass 
             
     def write_json(self):
         with open("storage.json", 'w') as file: json.dump(self.data, file, indent= 4)
